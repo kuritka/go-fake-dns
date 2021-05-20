cc51b89de0b80669533c3cbe4ee1dab2fd54837a
kuritka <kuritka@gmail.com>
2021-05-20 16:25:02 +0200

    fixed syncgronisation

--- dns_fake_core.go
+++ dns_fake_core.go
@@ -2,74 +2,108 @@ package exdns
 
 import (
 	"fmt"
+	"net"
 	"strings"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
-
 	"github.com/miekg/dns"
 )
 
+type DNSFakeSettings struct {
+	Port int
+	EdgeDNSZoneFQDN string
+	DNSZoneFQDN     string
+}
 
 // DNSMock acts as DNS server but returns mock values
 type DNSMock struct {
-	// ReadinessProbe is the channel that is released when the dns server starts listening
-	ReadinessProbe chan interface{}
+	// readinessProbe is the channel that is released when the dns server starts listening
+	readinessProbe chan interface{}
+	done chan interface{}
 	settings       DNSFakeSettings
-	done           chan interface{}
 	records        map[uint16][]dns.RR
-	server 			*dns.Server
+	server         *dns.Server
 	t              *testing.T
+	err 			error
 }
 
 func NewDNSFake(t *testing.T, settings DNSFakeSettings) *DNSMock {
 	return &DNSMock{
 		settings:       settings,
-		done:           make(chan interface{}),
-		ReadinessProbe: make(chan interface{}),
+		readinessProbe: make(chan interface{}),
+		done: 			make(chan interface{}),
 		records:        make(map[uint16][]dns.RR),
-		server: 		&dns.Server{Addr: fmt.Sprintf("[::]:%v", settings.Port), Net: "udp", TsigSecret: nil, ReusePort: false},
+		server:         &dns.Server{Addr: fmt.Sprintf("[::]:%v", settings.Port), Net: "udp", TsigSecret: nil, ReusePort: false},
 		t:              t,
 	}
 }
 
 func (m *DNSMock) Start() *DNSMock {
 	go func() {
-		err := m.listen()
-		require.NoError(m.t, err)
+		m.err = m.listen()
 	}()
-	<-m.ReadinessProbe
-	fmt.Printf("fake DNS listening on port %v \n", m.settings.Port)
+	<-m.readinessProbe
+	fmt.Printf("FakeDNS listening on port %v \n", m.settings.Port)
 	return m
 }
 
-func (m *DNSMock) RunTestFunc(f func()) {
-	defer m.stop()
-	f()
+func (m *DNSMock) RunTestFunc(f func()) error {
+	if m.err != nil {
+		f()
+		m.err = m.server.Shutdown()
+	}
+	return m.err
 }
 
+func (m *DNSMock) AddTXTRecord(fqdn string, strings ...string) *DNSMock {
+	t := &dns.TXT{
+		Hdr: dns.RR_Header{Name: fqdn, Rrtype: dns.TypeTXT, Class: dns.ClassINET, Ttl: 0},
+		Txt: strings,
+	}
+	m.records[dns.TypeTXT] = append(m.records[dns.TypeTXT], t)
+	return m
+}
+
+func (m *DNSMock) AddNSRecord(fqdn, nsName string) *DNSMock {
+	ns := &dns.NS{
+		Hdr: dns.RR_Header{Name: fqdn, Rrtype: dns.TypeNS, Class: dns.ClassINET, Ttl: 0},
+		Ns:  nsName,
+	}
+	m.records[dns.TypeNS] = append(m.records[dns.TypeNS], ns)
+	return m
+}
+
+func (m *DNSMock) AddARecord(fqdn string, ip net.IP) *DNSMock {
+	rr := &dns.A{
+		Hdr: dns.RR_Header{Name: fqdn, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0},
+		A:   ip.To4(),
+	}
+	m.records[dns.TypeA] = append(m.records[dns.TypeA], rr)
+	return m
+}
+
+func (m *DNSMock) AddAAAARecord(ip net.IP) *DNSMock {
+	rr := &dns.A{
+		Hdr: dns.RR_Header{Name: m.settings.DNSZoneFQDN, Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: 0},
+		A:   ip.To16(),
+	}
+	m.records[dns.TypeAAAA] = append(m.records[dns.TypeAAAA], rr)
+	return m
+}
 
 func (m *DNSMock) listen() (err error) {
 	dns.HandleFunc(m.settings.EdgeDNSZoneFQDN, m.handleReflect)
-	for e :=  range m.serve(m.done) {
+	for e := range m.serve() {
 		if e != nil {
-			close(m.done)
 			err = fmt.Errorf("%s%s", err, e)
 		}
 	}
 	return
 }
 
-func (m *DNSMock) stop() {
-	err := m.server.Shutdown()
-	if err != nil {
-		close(m.done)
-	}
-}
-
 func (m *DNSMock) startReadinessProbe() {
-	defer close(m.ReadinessProbe)
+	defer close(m.readinessProbe)
 	for i := 0; i < 5; i++ {
 		g := new(dns.Msg)
 		host := fmt.Sprintf("localhost:%v", m.settings.Port)
@@ -82,29 +116,26 @@ func (m *DNSMock) startReadinessProbe() {
 		return
 	}
 	// waiting too long, close listening
-	fmt.Println("->closing done readiness")
 	close(m.done)
 }
 
-func (m *DNSMock) serve(done <-chan interface{}) <-chan error {
+func (m *DNSMock) serve() <-chan error {
 	errors := make(chan error)
 	go func() {
 		defer close(errors)
 		var err error
 		go m.startReadinessProbe()
 		if err = m.server.ListenAndServe(); err != nil {
-			fmt.Println("listen serve error", err)
-			err = fmt.Errorf("Failed to setup the server: %s\n", err.Error())
+			errors <- fmt.Errorf("failed to setup the server: %s\n", err.Error())
+			close(m.done)
 		}
 		select {
-		case errors <- err:
-		case <-done:
+		case <-m.done:
 			return
 		}
 	}()
 	return errors
 }
-
 
 func (m *DNSMock) handleReflect(w dns.ResponseWriter, r *dns.Msg) {
 	msg := new(dns.Msg)
--- dns_server_test.go
+++ dns_server_test.go
@@ -3,6 +3,7 @@ package exdns
 import (
 	"fmt"
 	"github.com/miekg/dns"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"net"
 	"testing"
@@ -14,12 +15,14 @@ const (
 	port = 7753
 )
 
-func TestFake(t *testing.T) {
-	NewDNSFake(t, DNSFakeSettings{
-		Port:            port,
-		EdgeDNSZoneFQDN: "example.com.",
-		DNSZoneFQDN:     "cloud.example.com.",
-	}).
+var testSettings = DNSFakeSettings{
+	Port:            port,
+	EdgeDNSZoneFQDN: "example.com.",
+	DNSZoneFQDN:     "cloud.example.com.",
+}
+
+func TestDNSFake(t *testing.T) {
+	err := NewDNSFake(t, testSettings).
 		AddNSRecord("blah.cloud.example.com.","gslb-ns-us-cloud.example.com.").
 		AddNSRecord("blah.cloud.example.com.","gslb-ns-uk-cloud.example.com.").
 		AddNSRecord("blah.cloud.example.com.","gslb-ns-eu-cloud.example.com.").
@@ -35,34 +38,42 @@ func TestFake(t *testing.T) {
 			require.NoError(t, err)
 			require.NotEmpty(t, a.Answer)
 		})
+	assert.NoError(t, err)
 }
 
 // TestFakeN runs DNSFake several 10 times
-func TestFakeN(t *testing.T) {
+func TestDNSFakeN(t *testing.T) {
 	for i := 1; i< 10; i++ {
-		NewDNSFake(t, DNSFakeSettings{
-			Port:            port,
-			EdgeDNSZoneFQDN: "example.com.",
-			DNSZoneFQDN:     "cloud.example.com.",
-		}).
+		NewDNSFake(t, testSettings).
 			AddARecord("localtargets-roundrobin.cloud.example.com.", net.IPv4(10, 1, 0, 3)).
 			AddARecord("localtargets-roundrobin.cloud.example.com.", net.IPv4(10, 1, 0, 2)).
 			AddARecord("localtargets-roundrobin.cloud.example.com.", net.IPv4(10, 1, 0, 1)).
 			AddTXTRecord("localtargets-heartbeat-us.cloud.example.com.", "5m").
 			Start().
 			RunTestFunc(func() {
-				fmt.Println("Test ",i)
+				fmt.Println("FakeDNS test: ",i)
 				g := new(dns.Msg)
 				g.SetQuestion("localtargets-roundrobin.cloud.example.com.", dns.TypeA)
 				// put server under load....
-				for i := 0; i <= 30; i++ {
+				for i := 0; i <= 20; i++ {
 					a, err := dns.Exchange(g, fmt.Sprintf("%s:%v", server, port))
 					require.NoError(t, err)
 					require.NotEmpty(t, a.Answer)
 					require.Equal(t, 3, len(a.Answer))
-					time.Sleep(10 * time.Millisecond )
+					time.Sleep(5 * time.Millisecond )
 				}
-
 			})
 	}
+}
+
+func TestFakeDNSPortIsAlreadyInUse(t *testing.T) {
+	s := &dns.Server{Addr: fmt.Sprintf("[::]:%v", port), Net: "udp", TsigSecret: nil, ReusePort: false}
+	go func() { _ = s.ListenAndServe()}()
+	time.Sleep(100*time.Millisecond)
+	err :=NewDNSFake(t,testSettings).
+		Start().
+		RunTestFunc(func() {
+			fmt.Println("doing something...")
+		})
+	assert.Error(t, err)
 }
